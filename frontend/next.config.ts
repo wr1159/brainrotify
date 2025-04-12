e2f24abbc125888d40e5ad2a78235bfa7f412b44
wr1159 <chuweiron18@gmail.com>
2025-04-12 20:40:35 +0100

    Fix pinopretty

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    webpack: (config) => {
+        config.externals.push("pino-pretty", "lokijs", "encoding");
+        return config;
+    },
 };
 
 export default nextConfig;
